d0ce6b4db9d7e9085f6dac11eea4463046d51d71
Carlagoes <carllagoess@gmail.com>
2023-04-12 22:09:25 -0300

    criação dos projetos criação das propriedades

---
+++ app/IEscola.Domain/Entities/Professor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IEscola.Domain.Entities
+{
+    public class Professor
+    {
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Cpf { get; private set; }
+        public DateTime? DataNascimento { get; private set; }
+        public DateTime? DataCadastro { get; private set; }
+        public bool Ativo { get; private set; }
+
+
+        public Professor(int id, string nome, string cpf, DateTime? dataNascimento)
+        {
+            Id = id;
+            Nome = nome;
+            Cpf = cpf;
+            DataNascimento = dataNascimento;
+            DataCadastro = DateTime.Now;
+            Ativo = true;
+        }
+
+        public void Inativar()
+        {
+            Ativo = false;
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
